36f57979d8c21b428625ae787d4c73bef82debbc
Eddie Yeung <eycs0317@gmail.com>
2021-11-08 16:59:26 -0800

    got th api key

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+
+// import './App.css';
+import React from 'react'
+import './header.css'
+
+function Header() {
+  return (
+    <div className='header'>
+      <img src='https://cdn.icon-icons.com/icons2/1476/PNG/512/yelp_101766.png' alt='yelp-icon'></img>
+      <p className='header-text'>Yelp</p>
+    </div>
+  );
+}
+
+export default Header;
